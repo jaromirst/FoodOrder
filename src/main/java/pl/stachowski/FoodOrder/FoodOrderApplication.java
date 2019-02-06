82994f902cb82bad4e93f58c73d2f8766ff0454f
Jarek <stachowski.jarek@wp.pl>
2019-02-06 17:48:01 +0100

    Initial commit

---
+++ src/main/java/pl/stachowski/FoodOrder/FoodOrderApplication.java
@@ -0,0 +1,14 @@
+package pl.stachowski.FoodOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderApplication.class, args);
+	}
+
+}
+
